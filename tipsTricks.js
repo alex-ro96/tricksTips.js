3757623fa3048b2c08990a13262da506be886433
Alexandra <alexandra.chamero-rossy@hotmail.com>
2022-04-19 14:26:50 +0200

    First trick

---
+++ tipsTricks.js
@@ -0,0 +1,8 @@
+// for a random number for example between 0 and 100: 
+
+var a = math.random() * 101;
+a = math.floor(a);
+
+// ou alors on peut simplifier:
+
+var a = Math.floor(Math.random() * 101); 
