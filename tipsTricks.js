e2ab0dc7f5080e60423e426394be3f634b15c8db
Alexandra <alexandra.chamero-rossy@hotmail.com>
2022-04-19 14:30:15 +0200

    Trying to see it in terminal

--- tipsTricks.js
+++ tipsTricks.js
@@ -1,8 +1,13 @@
 // for a random number for example between 0 and 100: 
 
-var a = math.random() * 101;
-a = math.floor(a);
+var a = Math.random() * 101;
+a = Math.floor(a);
 
 // ou alors on peut simplifier:
 
 var a = Math.floor(Math.random() * 101); 
+
+console.log(a);
+console.log(a);
+console.log(a);
+console.log(a);
